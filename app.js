061b33d3d60f6b9df78f83d780fce3d3111ba67b
sunilkarki <sunil@pop-os.localdomain>
2023-09-29 12:02:35 +0545

    single post and delete operation

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended:true}))
 
 app.get("/",async(req,res)=>{
     const allPosts=await posts.findAll()  //posts vanne table bata sab find garney
-    console.log(allPosts) //yo chai array vitra object vara aauxa
+    // console.log(allPosts) //yo chai array vitra object vara aauxa
     res.render("allPost",{posts:allPosts})//we pass it here so we can display it in our  / page mabecoz ejs file
 })
 app.get("/newPost",(req,res)=>{
@@ -32,7 +32,7 @@ app.get("/newPost",(req,res)=>{
 //for submit action api hit post
 app.post("/newPost",async(req,res)=>{
 
-    console.log(req.body)
+    // console.log(req.body)
     const {title,subtitle,description}=req.body //data of form store greko
 
     // aba databse ma hamro data HTMLAnchorElement
@@ -46,6 +46,38 @@ app.post("/newPost",async(req,res)=>{
 
 })
 
+app.get("/singlePost/:id",async(req,res)=>{
+    // console.log(req.params.id)
+    const id=req.params.id
+    const post=await posts.findAll({
+        where:{
+            id:id
+        }
+    })
+    // console.log(post)
+
+
+    res.render("singlePost",{post:post})
+
+})
+
+app.get("/delete/:id",async(req,res)=>{
+
+    console.log(req.params.id)
+    const id=req.params.id
+    const deletePost=await posts.destroy({
+        where:{
+            id:id
+        }
+        
+    })
+    console.log("deletion succcessfull")
+        res.redirect("/")
+
+
+
+
+})
 
 app.listen(3000,(req,res)=>{
     console.log("Node has started at port 3000 ")
